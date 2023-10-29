953ab4d043850c52fb6e6dc14e495dcfbccc1751
aliyou <a_yousefi78@yahoo.com>
2023-10-29 13:01:04 +0330

    license added

---
+++ lib/model/error_model.dart
@@ -0,0 +1,10 @@
+
+enum ErrorStatus {server, timeout, socket, unknown, none, noContent}
+
+class ErrorModel {
+  final String title;
+  final String? subtitle;
+  final ErrorStatus errorStatus;
+
+  ErrorModel({required this.title, this.subtitle, required this.errorStatus});
+}
